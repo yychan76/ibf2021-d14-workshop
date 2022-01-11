250339a209535467bc283b0557992a05edf15e7b
Chan Yew Yen <chan.yewyen@gmail.com>
2022-01-11 15:04:43 +0800

    Fix package name

--- src/main/java/nus/edu/sg/workshop14/error/ResourceNotFoundException.java
+++ src/main/java/nus/edu/sg/workshop14/error/ResourceNotFoundException.java
@@ -1,4 +1,4 @@
-package nus.edu.sg.workshop13.error;
+package nus.edu.sg.workshop14.error;
 
 public class ResourceNotFoundException extends RuntimeException {
     private static final long serialVersionUID = 1L;
