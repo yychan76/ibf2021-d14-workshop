3ba41643a0b120ccba9ae7be8506b306cd8c2e2e
Chan Yew Yen <chan.yewyen@gmail.com>
2022-01-11 16:05:26 +0800

    Bugfix NativePRNG class invalid for deserialization

--- src/main/java/nus/edu/sg/workshop14/model/Contact.java
+++ src/main/java/nus/edu/sg/workshop14/model/Contact.java
@@ -7,6 +7,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class Contact implements Serializable {
+    private static final long serialVersionUID = 1L;
     private String name;
     private String email;
     private String phone;
